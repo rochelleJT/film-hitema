00517bfc2c096a451ab98605faff6c885be4464c
rochelleJT <rochelle.theva@gmail.com>
2018-09-26 13:51:47 +0200

    affichages des données + détail de chaque ligne

---
+++ film_detail.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+<?php
+
+	$id = $_GET['_id'];
+	$user = "rochelle";
+	$pwd = 'Azerty01';
+	$mongo = new MongoDB\Driver\Manager("mongodb://${user}:${pwd}@ds113923.mlab.com:13923/films-hitema");
+	
+	$filter = ['_id' => new MongoDB\BSON\ObjectID($id)];
+	$options = [];
+
+	$query = new MongoDB\Driver\Query($filter, $options);
+
+	$cursor = $mongo->executeQuery('films-hitema.films', $query);
+	
+	foreach ($cursor as $document) {
+		$document = json_decode(json_encode($document),true);echo "<table>";
+		echo "<tr><th>Année</th><td>" . $document['Year']. "</td></tr>";
+		echo "<tr><th>Durée</th><td>" . $document['Length']. "</td></tr>";
+		echo "<tr><th>Titre</th><td>" . $document['Title']. "</td></tr>";
+		echo "<tr><th>Genre</th><td>" . $document['Subject']. "</td></tr>";
+		echo "<tr><th>Acteurs</th><td>" . $document['Actor']. "</td></tr>";
+		echo "<tr><th>Director</th><td>" . $document['Director']. "</td></tr>";
+		echo "<tr><th>Popularité</th><td>" . $document['Popularity']. "</td></tr>";
+		echo "<tr><th>Award</th><td>" . $document['Awards']. "</td></tr>";
+		echo "<tr><th>Image</th><td>" . $document['*Image']. "</td></tr>";
+		echo "</table>";
+	}
+	
+
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,30 @@
+<html>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+</head>
+<body>
 <?php
-   // connect to mongodb
 	$user = "rochelle";
 	$pwd = 'Azerty01';
-	//$db = 'films-hitema';
-	$collection = 'films';
-	
 	$mongo = new MongoDB\Driver\Manager("mongodb://${user}:${pwd}@ds113923.mlab.com:13923/films-hitema");
-	echo "Connection to database successfully";
 	
-	$filter = [];
+	$filter = ['Year' => ['$gt' => 1900]];
 
 	$options = [
 		'sort' => ['_id' => -1],
@@ -18,12 +34,14 @@
 
 	$cursor = $mongo->executeQuery('films-hitema.films', $query);
 	
+	echo "<br><br><table><tr><th>Année</th><th>Titre</th><th>Genre</th><th>Acteurs</th><th>Actrices</th><th>Directeur</th></tr>";
 	foreach ($cursor as $document) {
-		var_dump($document);
+		$document = json_decode(json_encode($document),true);
+		echo "<tr>";
+		echo "<td>" . $document['Year']. "</td><td><a href='film_detail.php?_id=".implode(" ",$document['_id'])."'>" . $document['Title']. "</a></td><td>". $document['Subject']. "</td><td>". $document['Actor']. "</td><td>". $document['Actress']. "</td><td>". $document['Director']. "</td>";
+		echo "</tr>";
 	}
-	
-   // select a database
-   //$db = $m->mydb;
-	
-   //echo "Database mydb selected";
+	echo "</table>";
 ?>
+</body>
+</html>
